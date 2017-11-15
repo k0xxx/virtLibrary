6c3a60bcc3b48462bff86a4f705d0ba06b2a61fe
k0xxx <androidk0xxx@gmail.com>
2017-11-15 18:43:55 +0200

    first changes to colleagial

---
+++ src/components/mainTopBar/mainTopBar.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="mainTopBar">
+		
+	</div>
+</template>
+<script>
+//import { baseAPI } from '../../config.js';
+export default{
+	name: 'mainTopBar',
+	data() {
+		return {
+            
+		}
+	},
+	//components: {newMessageModal},
+	//props: ['contact', 'contactStatus', 'isSubscribe'],
+	
+}  
+</script>
+<style>
+
+</style>
---
+++ src/components/profile/profile.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="profile">
+		
+	</div>
+</template>
+<script>
+//import { baseAPI } from '../../config.js';
+export default{
+	name: 'profile',
+	data() {
+		return {
+            
+		}
+	},
+	//components: {newMessageModal},
+	//props: ['contact', 'contactStatus', 'isSubscribe'],
+	
+}  
+</script>
+<style>
+
+</style>
